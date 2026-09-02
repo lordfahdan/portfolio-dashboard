527a179ca34398b8d5767e8b89ab5dc51f7c613d
Fahdan Khairi <59869319+lordfahdan@users.noreply.github.com>
2026-09-02 11:37:23 +0700

    Add visitor tracking to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,12 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 
+import { trackVisitor } from "./analytics/visitor";
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>
 );
+
+trackVisitor();
